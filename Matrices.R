fcda4c9f0ef05ec6cf8e84f7abe226d706d6abb9
Michael Lee <selicas@gmail.com>
2017-03-07 14:50:30 -0800

    Updated matrices

--- Matrices.R
+++ Matrices.R
@@ -6,3 +6,22 @@ matrix(v)
 matrix(v,nrow = 2)
 matrix(1:12,byrow = FALSE,nrow=4)
 matrix(1:12,byrow = TRUE,nrow=4)
+goog <- c(450,451,452,445,468)
+msft <- c(230,231,232,233,220)
+
+stocks <- c(goog,msft)
+stock.matrix <- matrix(stocks,byrow=T,nrow=2)
+days <- c('Mon','Tue','wed','thu','fri')
+st.names <- c('Goog','MSFT')
+colnames(stock.matrix) <- days
+rownames(stock.matrix) <- st.names
+print(stock.matrix)
+
+mat <- matrix(1:25,byrow=T,nrow=5)
+mat*2
+Mat/2
+1/mat
+mat>15
+mat[mat>15]
+mat+mat
+mat%*%mat
