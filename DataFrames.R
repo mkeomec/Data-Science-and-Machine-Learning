4c0cb9b10d4d5ac609f90f15e3abf14f212ab4bb
Michael Lee <selicas@gmail.com>
2017-03-23 16:34:54 -0700

    Completed Matrices. Started Data frames

---
+++ DataFrames.R
@@ -0,0 +1,14 @@
+state.x77
+USPersonalExpenditure
+women
+data()
+DNase
+head(state.x77)
+str(state.x77)
+summary(state.x77)
+days <- c('Mon','Tues','Wed','Thur','Fri')
+temp <- c(22.2,21,23,24.3,25)
+rain <- c(T,T,F,F,T)
+df <- data.frame(days,temp,rain)
+str(df)
+summary(df)
