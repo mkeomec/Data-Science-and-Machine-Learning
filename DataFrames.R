98278a7e86768eb4506b00e0d494be24b72c0214
Michael Lee <selicas@gmail.com>
2017-05-12 16:25:03 -0700

    progress on dataframe lecture

--- DataFrames.R
+++ DataFrames.R
@@ -12,3 +12,16 @@ rain <- c(T,T,F,F,T)
 df <- data.frame(days,temp,rain)
 str(df)
 summary(df)
+df[1,]
+df[,1]
+df[1:5,c('days','temp')]
+df$days
+df$temp
+df['days']
+subset(df,subset=rain==TRUE)
+subset(df,subset=temp>23)
+sorted.temp <- order(df['temp'])
+df[sorted.temp,]
+desc.temp <- order(-df['temp'])
+df[desc.temp,]
+desc.temp <- order(-df$temp)
