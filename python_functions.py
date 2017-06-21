8f78202562bc6268755abac39aa47eae2f511291
Michael Lee <selicas@gmail.com>
2017-06-20 17:26:00 -0700

    Python functions

--- python_functions.py
+++ python_functions.py
@@ -1,9 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+#Loops
 
-This is a temporary script file.
-"""
+for x in range(5):
+    print(x)
+
+for x in range(3,6):
+    print(x)
+    
+numbers = [
+    951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,
+    615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+    958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 380, 126, 721, 328, 753, 470,
+    743, 527
+]
+
+for number in numbers:
+    if number==237:
+        break
+    if number % 2 ==1:
+        continue
+    print(number)
+
+
+
+
+
+#Functions
+
+
 
 def my_function():
     print("Hello From My Function!")
@@ -30,3 +56,117 @@ def name_the_benefits_of_functions():
         print(build_sentence(benefit))
 
 name_the_benefits_of_functions()
+
+# Classes and objects
+
+class MyClass:
+    variable="blah"
+    
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+myobjectx.variable
+
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+myobjecty.variable = "yackity"
+
+print(myobjectx.variable)
+print(myobjecty.variable)
+
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+myobjectx.function()
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+car1 = Vehicle()
+car1.name="Fer"
+car1.color='red'
+car1.kind="convertible"
+car1.value=60000.00
+car1.description()
+
+car2 = Vehicle()
+car2.name="Jump"
+car2.color='blue'
+car2.kind="van"
+car2.value=10000.00
+car2.description()
+
+# test code
+print(car1.description())
+print(car2.description())
+
+
+
+#Dictionaries
+phonebook = {}
+phonebook["John"] = 938477566
+phonebook["Jack"] = 938377264
+phonebook["Jill"] = 947662781
+print(phonebook)
+
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+print(phonebook)
+
+phonebook = {"John" : 938477566,"Jack" : 938377264,"Jill" : 947662781}
+for name, number in phonebook.items():
+    print("Phone number of %s is %s" % (name, number))
+
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+del phonebook["John"]
+print(phonebook)
+
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+phonebook.pop("John")
+print(phonebook)
+
+#Exercise
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+
+phonebook["Jake"]=938273443
+phonebook.pop("Jill")
+
+if "Jake" in phonebook:
+    print("Jake is listed in the phonebook.")
+if "Jill" not in phonebook:
+    print("Jill is not listed in the phonebook.")
